8b40f340ac3a886a343755a8044e1dbf4c65189d
Frontend <sweetysks23@gmail.com>
2023-10-30 22:24:24 +0900

    react-query

---
+++ src/components/Products.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+export default function Products() {
+  const [checked, setChecked] = useState(false);
+  const {isLoading, error, data:products} 
+    = useQuery(['products', checked], async ()=>{
+    console.log('fetching...', checked)
+    return fetch(`data/${checked ? 'sale_' : ''}products.json`)
+    .then((res) => res.json())
+  },{
+    staleTime: 1000 * 60 * 50,
+  })
+ 
+  const handleChange = ()=> setChecked(prev => !prev)
+
+  
+  if (isLoading) return <p>Loading...😎</p>
+  
+  if (error) return <p>{error}</p>
+  return (
+    <>
+      <input id='checkbox' type="checkbox" value={checked} onChange={handleChange} />
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
